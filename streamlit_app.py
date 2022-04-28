319f92ef33604d146d2eb6b86acc1bfc0c2b8441
Ophir Prusak <prusak@gmail.com>
2022-04-28 18:00:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,11 @@ streamlit.text('Scrambled Eggs')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# Add a picklist for the fruit
+streamlit.multiselect("Pick your fruit:", list(my_fruit_list.index))
+
+#display the table
 streamlit.dataframe(my_fruit_list)
 
