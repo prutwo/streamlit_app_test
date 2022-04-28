4ce03595360c72c5c40dfea73ee6267c115440cb
Ophir Prusak <prusak@gmail.com>
2022-04-28 17:52:34 -0400

    adding more text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('Jacks Healthy Diner')
+streamlit.header('Breakfast menu')
+streamlit.text('Blueberry Oatmeal')
+streamlit.text('Kale smoothie')
+streamlit.text('Scrambled Eggs')
